54cac52ef762c83cda7b21a33f6ddace270591e0
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-07-25 22:45:12 -0300

    275 finished

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -10,8 +11,14 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   @Input()
   public price: number = 0
 
+  public interval$?: Subscription
+
+
   ngOnInit(): void {
     console.log('Componente HIJO: ngOnInit ')
+
+     this.interval$ = interval(1000).subscribe( value => console.log(`tick ${value}`) )
+
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -21,6 +28,7 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
 
   ngOnDestroy(): void {
     console.log('Componente HIJO: ngOnDestroy')
+    this.interval$?.unsubscribe()
   }
 
 
